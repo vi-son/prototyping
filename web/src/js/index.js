83f718b32969cfc5655864aa1e228228f9cf318a
Guido Schmidt <git@guidoschmidt.cc>
2020-06-30 23:21:41 +0200

    WIP/web: styling audio player

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -2,64 +2,11 @@
 /// Check: https://github.com/goldfire/howler.js
 import React, { useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
-import Vue from "vue";
 
-const audioFile = require("../assets/audio/patterns.202006.mp3");
+import AudioPlayer from "./components/AudioPlayer.js";
+
+const audioFile = require("../../assets/audio/patterns.202006.mp3");
 let audioCtx = new AudioContext();
-
-class AudioPlayer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.audio = React.createRef();
-    this.timeUpdate = this.timeUpdate.bind(this);
-    this.changeTime = this.changeTime.bind(this);
-    this.state = {
-      duration: NaN,
-      currentTime: 0
-    };
-  }
-
-  timeUpdate() {
-    this.setState({
-      currentTime: this.audio.current.currentTime,
-      duration: this.audio.current.duration
-    });
-  }
-
-  changeTime(e) {
-    const percent = e.clientX / 500;
-    this.audio.current.currentTime = this.state.duration * percent;
-  }
-
-  render() {
-    return (
-      <div className="audio-player">
-        <audio preload="true" onTimeUpdate={this.timeUpdate} ref={this.audio}>
-          <source src={this.props.audiosrc} />
-        </audio>
-        <div onClick={() => this.audio.current.play()}>Play</div>
-        <div onClick={() => this.audio.current.pause()}>Pause</div>
-        <div
-          className="timeline"
-          style={{ width: 500, height: 5, background: "black" }}
-          onClick={this.changeTime}
-        >
-          <div
-            className="playhead"
-            style={{
-              width: 5,
-              height: 5,
-              background: "red",
-              position: "relative",
-              left: (this.state.currentTime / this.state.duration) * 500
-            }}
-          ></div>
-        </div>
-        {this.state.currentTime}/{this.state.duration}
-      </div>
-    );
-  }
-}
 
 const mount = document.querySelector("#mount");
 ReactDOM.render(
