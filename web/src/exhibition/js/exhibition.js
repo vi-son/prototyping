45c5628a87329a97db4d5a0c2b0bf0cc86f1607c
Guido Schmidt <git@guidoschmidt.cc>
2020-09-06 10:35:58 +0200

    ADD/web/src/exhibition: added prototype for exhibition space

---
+++ web/src/exhibition/js/exhibition.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>Exhibition Prototype</h1>;
+};
+
+const mount = document.querySelector("#mount");
+ReactDOM.render(<App />, mount);
